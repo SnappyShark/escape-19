7fff6ffef8617ca7539cdf7110c6166d7ce603dd
SnappyShark <cottonian99@gmail.com>
2020-04-22 17:56:58 +0530

    Update v2

---
+++ webpage/admin/approval/approvalreq.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Generated Report</title>
+	<link rel="stylesheet" type="text/css" href="approvalreq.css">
+    <div id="topbar">
+            <a  href=""><button id="log">Log Out</button></a>
+        </div>
+</head>
+<body>
+<span id="logoutlink"><a href="#">Logout</a></span>
+<br>
+<br>
+<div class="wrap">
+<center><h1>Customer Monitoring Portal</h1>
+<h3>LIST</h3>
+<p>List from DB goes into here...</p>
+</center>
+    </div>
+<a href="dashboard.html"><button id ="back">Back</button></a>
+</body>
+
+</html>
