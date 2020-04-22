ac1bb3ce683cb71f11b3cd1fc07244f4da5b71ca
SnappyShark <cottonian99@gmail.com>
2020-04-22 18:46:24 +0530

    Finished Report and Add Customer

---
+++ webpage/portal/generaterep.php
@@ -0,0 +1,29 @@
+<?php
+$svr = '127.0.0.1';
+$usr = 'root';
+$password = '';
+$db = 'escape';
+
+$conn = new mysqli($svr, $usr, $password, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "SELECT * FROM log_cust_data ORDER BY date,time desc";
+$result = $conn->query($sql);
+
+print "<table border=3><tr><th>Phone</th><th>Date</th><th>Time</th></tr>";
+
+while($row = $result->fetch_assoc()) {
+
+    $t = $row['phone_num'];
+    echo "<tr><td>".$row['phone_num']."</td><td>".$row['date']."</td><td>".$row['time']."</td></tr>";
+}
+
+echo "</table>";
+
+
+$conn->close();
+?>
---
+++ webpage/portal/userentry.php
@@ -0,0 +1,27 @@
+<?php
+
+$svr = '127.0.0.1';
+$usr = 'root';
+$password = '';
+$db = 'escape';
+
+$conn = new mysqli($svr, $usr, $password, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$phone_num = $_POST['phone_num'];
+
+$date = date("Y/m/d");
+date_default_timezone_set("Asia/Kolkata");
+$time = date("h:i");
+
+echo "Processing";
+
+$sql = "INSERT INTO log_cust_data VALUES($phone_num, '$date', '$time')";
+$result = $conn->query($sql);
+
+
+echo "<meta http-equiv='refresh' content='1;url=userentry.html'>"
+?>
